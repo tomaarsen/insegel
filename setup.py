45f009eab81f3086b43e3fc1dfd1b5cf670088ef
Tom Aarsen <Cubiegamedev@gmail.com>
2021-10-10 23:05:54 +0200

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nltk_theme',
-    version='2.0.2',
+    version='2.0.3',
     url='https://github.com/tomaarsen/nltk_theme',
     license='MIT',
     author='Mika Naylor (Autophagy), Tom Aarsen',
