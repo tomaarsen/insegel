7f3fe2abbae550ae450633c6495362fe27857a45
Tom Aarsen <Cubiegamedev@gmail.com>
2021-10-13 12:02:52 +0200

    Create post-release fix as the css wasn't updated.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nltk_theme',
-    version='2.0.4',
+    version='2.0.4.post1',
     url='https://github.com/tomaarsen/nltk_theme',
     license='MIT',
     author='Mika Naylor (Autophagy), Tom Aarsen',
