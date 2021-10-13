956ed7815d2bd5bd268bc7d87055e357be1271b1
Tom Aarsen <Cubiegamedev@gmail.com>
2021-10-13 11:57:43 +0200

    Update to 2.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nltk_theme',
-    version='2.0.3',
+    version='2.0.4',
     url='https://github.com/tomaarsen/nltk_theme',
     license='MIT',
     author='Mika Naylor (Autophagy), Tom Aarsen',
